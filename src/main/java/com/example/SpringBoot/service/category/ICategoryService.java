87217c754aa70ebf996fb5b14d9f6e4f5a44b31e
NetflixClone <hosiduc2002.chem@gmail.com>
2024-10-06 23:14:14 +0700

    feat: Add Category Service

---
+++ src/main/java/com/example/SpringBoot/service/category/ICategoryService.java
@@ -0,0 +1,15 @@
+package com.example.SpringBoot.service.category;
+
+import com.example.SpringBoot.model.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    Category getCategoryById(Long id);
+    Category getCategoryByName(String name);
+    List<Category> getAllCategories();
+    Category addCategory(Category category);
+    Category updateCategory(Category category, Long id);
+    void deleteCategory(Long id);
+
+}
